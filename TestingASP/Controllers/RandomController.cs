30fc7c41442527ad069eb4e55dbd548d41881858
Michael Perdue <michael0perdue@gmail.com>
2024-01-13 19:55:34 +0000

    made a basic api in asp

---
+++ TestingASP/Controllers/RandomController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestingASP.Controllers;
+[ApiController]
+[Route("api/Random")]
+public class RandomController : ControllerBase
+{
+        private readonly ILogger<RandomController> _logger;
+        private Dictionary<int, RandomData> data;
+
+        public RandomController(ILogger<RandomController> logger)
+        {
+            data = new Dictionary<int, RandomData>();
+            for (int i = 0; i < 5; i++)
+            {
+                data.Add(i,new RandomData("test",i,Random.Shared.Next().ToString()));
+            }
+            _logger = logger;
+        }
+
+        [HttpGet("{id}",Name = "GetAnotherData")]
+        [ProducesResponseType(typeof(RandomData), 200)]
+        public IActionResult Get(string id)
+        {
+            return Ok(data[int.Parse(id)]);
+        }
+}
---
+++ TestingASP/RandomData.cs
@@ -0,0 +1,15 @@
+﻿namespace TestingASP;
+
+public class RandomData
+{
+    public int Id { get; }
+    public string Message { get; set; }
+    public string? Value2 { get; }
+
+    public RandomData(string message, int id, string? value2 = null)
+    {
+        Message = message;
+        Id = id;
+        Value2 = value2;
+    }
+}
