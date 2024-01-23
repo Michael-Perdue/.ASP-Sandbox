407dab7b8aadb631ef7f55e9181b731637a8c1b2
Michael Perdue <michael0perdue@gmail.com>
2024-01-23 10:32:50 +0000

    added further unit tests and refactored the code

--- Testing/LoginControllerTests.cs
+++ Testing/LoginControllerTests.cs
@@ -33,4 +33,22 @@ public class LoginControllerTests
         Assert.Equal(200, (result as OkObjectResult)?.StatusCode);
     }
     
+    public void GetRandom_Returns_OkResult()
+    {
+        Mock<ILogger<LoginController>> loggerMock = new Mock<ILogger<LoginController>>();
+        var testData = new Dictionary<int, RandomData>();
+        testData.Add(1,new RandomData("Testing", 1, "RandomValue"));
+        LoginController controller = new LoginController(loggerMock.Object)
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
+        controller.data = testData; 
+        ActionResult result = controller.GetRandom("1");
+        Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(200, (result as OkObjectResult)?.StatusCode);
+    }
+    
 }
