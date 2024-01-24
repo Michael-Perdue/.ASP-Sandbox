da52536b4bc0de84988bbf545369d901a24a6db8
Michael Perdue <michael0perdue@gmail.com>
2024-01-24 13:28:04 +0000

    added more unit tests and started to add query context to the unit test

--- Testing/LoginControllerTests.cs
+++ Testing/LoginControllerTests.cs
@@ -4,6 +4,7 @@ using Xunit;
 using Microsoft.AspNetCore.Mvc;
 using TestingASP.Controllers;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 using Moq;
 using TestingASP;
 using Xunit;
@@ -14,41 +15,71 @@ namespace Testing;
 
 public class LoginControllerTests
 {
+
+    private LoginController GetNewController(QueryCollection queryCollection)
+    {
+        Mock<ILogger<LoginController>> loggerMock = new Mock<ILogger<LoginController>>();
+        return new LoginController(loggerMock.Object)
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    Request = { Query = queryCollection}
+                }
+            }
+        };
+    }
+
     [Fact]
     public void GetRandom_Returns_OkResult()
     {
-        Mock<ILogger<LoginController>> loggerMock = new Mock<ILogger<LoginController>>();
         var testData = new Dictionary<int, RandomData>();
         testData.Add(1,new RandomData("Testing", 1, "RandomValue"));
-        LoginController controller = new LoginController(loggerMock.Object)
-        {
-            ControllerContext = new ControllerContext
-            {
-                HttpContext = new DefaultHttpContext()
-            }
-        };
+        LoginController controller = GetNewController(new QueryCollection());
         controller.data = testData; 
         ActionResult result = controller.GetRandom("1");
         Assert.IsType<OkObjectResult>(result);
         Assert.Equal(200, (result as OkObjectResult)?.StatusCode);
     }
     
-    public void GetRandom_Returns_OkResult()
+    [Fact]
+    public void GetRandom_Returns_NotFoundResult()
     {
-        Mock<ILogger<LoginController>> loggerMock = new Mock<ILogger<LoginController>>();
-        var testData = new Dictionary<int, RandomData>();
-        testData.Add(1,new RandomData("Testing", 1, "RandomValue"));
-        LoginController controller = new LoginController(loggerMock.Object)
-        {
-            ControllerContext = new ControllerContext
-            {
-                HttpContext = new DefaultHttpContext()
-            }
-        };
-        controller.data = testData; 
-        ActionResult result = controller.GetRandom("1");
-        Assert.IsType<OkObjectResult>(result);
-        Assert.Equal(200, (result as OkObjectResult)?.StatusCode);
+        LoginController controller = GetNewController(new QueryCollection());
+        ActionResult result = controller.GetRandom("100");
+        Assert.IsType<NotFoundResult>(result);
+        Assert.Equal(404, (result as NotFoundResult)?.StatusCode);
     }
     
+    [Fact]
+    public void GetUser_Returns_NotFoundResult()
+    {
+        QueryCollection query = new QueryCollection(new Dictionary<string, StringValues>
+        {
+            ["id"] = "100",
+            ["user"] = "noaccount",
+            ["pass"] = "noaccount"
+        });
+        LoginController controller = GetNewController(query);
+        
+        ActionResult result = controller.GetUser("100");
+        Assert.IsType<NotFoundResult>(result);
+        Assert.Equal(404, (result as NotFoundResult)?.StatusCode);
+        result = controller.GetUser(id: null,user: "noaccount", pass: "noaccount");
+        Assert.IsType<NotFoundResult>(result);
+        Assert.Equal(404, (result as NotFoundResult)?.StatusCode);
+    }
+    
+    [Fact]
+    public void GetUserApiCall_Returns_NotFoundResult()
+    {
+        LoginController controller = GetNewController(new QueryCollection());
+        ActionResult result = controller.GetUser("100");
+        Assert.IsType<NotFoundResult>(result);
+        Assert.Equal(404, (result as NotFoundResult)?.StatusCode);
+        result = controller.GetUser(null,"noaccount","noaccount");
+        Assert.IsType<NotFoundResult>(result);
+        Assert.Equal(404, (result as NotFoundResult)?.StatusCode);
+    }
 }
